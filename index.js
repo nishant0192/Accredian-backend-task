1be88624746a1286b7d65299a36f6f73442d8796
Nishant <nishantgolakiya2001@gmail.com>
2024-07-06 19:09:45 +0530

    cors updated

--- index.js
+++ index.js
@@ -2,16 +2,22 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const userRoutes = require('./routes/userRoutes');
 const referralRoutes = require('./routes/referralRoutes');
-const programRoutes = require('./routes/programRoutes')
-const cors = require('cors')
+const programRoutes = require('./routes/programRoutes');
+const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Define CORS options
+const corsOptions = {
+  origin: ['http://localhost:5137', 'https://accredian-frontend-task-two-psi.vercel.app/'],
+  credentials: true,  // <-- Required to pass cookies
+};
+
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors())
+app.use(cors(corsOptions)); // Apply CORS with options
 app.use('/api/users', userRoutes);
 app.use('/api', referralRoutes);
 app.use('/api/program', programRoutes);
